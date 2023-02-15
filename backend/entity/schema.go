886422ba352f36f61f0a3dee8ce9cf65d70eb9d6
Maprang6224 <Maprang6224@gmail.com>
2023-02-15 10:38:04 -0800

    add valid customerID - close #4

--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -8,5 +8,5 @@ type Customer struct {
 	gorm.Model
 	Name       string `valid:"required~กรุณากรอกชื่อ"`
 	Email      string
-	CustomerID string
+	CustomerID string `valid:"required~CustermerID not match, matches((L|M|H)(1|2|3|4|5|6|7)(1|2|3|4|5|6|7)(1|2|3|4|5|6|7)(1|2|3|4|5|6|7)(1|2|3|4|5|6|7)(1|2|3|4|5|6|7)(1|2|3|4|5|6|7)$)~CustermerID not match"`
 }
